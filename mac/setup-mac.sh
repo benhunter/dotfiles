3cf399eddb806cce2c782a8827ce7c05452a594c
Ben Hunter <code@benhunter.me>
2023-05-14 11:28:45 -0500

    Add .ideavimrc for mac

--- mac/setup-mac.sh
+++ mac/setup-mac.sh
@@ -5,6 +5,7 @@ cp ./.zshrc $HOME/
 mkdir $HOME/bin
 ln -s $HOME/projects/scripts/*.sh $HOME/bin
 ln -s $HOME/projects/secrets/.secrets.zshrc $HOME/.secrets.zshrc
+ln -s $HOME/projects/dotfiles/mac/.ideavimrc $HOME/.ideavimrc
 
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
