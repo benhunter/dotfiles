308ed08f93f7694352895636fc8da181df9175dd
Ben Hunter <code@benhunter.me>
2023-05-29 00:34:53 -0500

    Remote rogue Brewfile

--- mac/setup-mac.sh
+++ mac/setup-mac.sh
@@ -10,10 +10,16 @@ ln -s $HOME/projects/secrets/.secrets.zshrc $HOME/.secrets.zshrc
 # Scripts
 mkdir $HOME/bin
 ln -s $HOME/projects/scripts/*.sh $HOME/bin
+# ln -s $SCRIPT_DIR/update-mac.sh $HOME/bin/update-mac.sh
+ln -s $SCRIPT_DIR/*.sh $HOME/bin/
 
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions pull
+
+# Tmux
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+$HOME/.tmux/plugins/tpm/bin/update_plugins all
 
 # Git
 cp $SCRIPT_DIR/.gitignore_global ~/.gitignore_global
--- mac/update-mac.sh
+++ mac/update-mac.sh
@@ -3,5 +3,10 @@
 brew upgrade # Homebrew
 omz update #oh-my-zsh
 
+
+git -C ~/.tmux/plugins/tpm/bin/update_plugins pull
+~/.tmux/plugins/tpm/bin/update_plugins all
+
+# TODO
 # NvChad
 ## Mason
