f38af7239134cedea4a0a122e9688cf0e19862eb
Ben Hunter <code@benhunter.me>
2023-05-29 00:37:27 -0500

    Backup

--- mac/backup-mac.sh
+++ mac/backup-mac.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 echo "Script Directory: $SCRIPT_DIR"
 
 echo "Dumping brew..."
-brew bundle dump --file=$SCRIPT_DIR/Brewfile
+brew bundle dump --file=$SCRIPT_DIR/Brewfile --force
 
 echo "Copying Neovim NvChad config..."
 cp -r ~/.config/nvim/lua/custom/ $SCRIPT_DIR/.config/nvim/lua/custom/
