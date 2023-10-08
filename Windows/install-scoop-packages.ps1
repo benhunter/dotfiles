62fe6bd915fd3b5f2cc4a85a8b046a84e0d1f387
Ben Hunter <code@benhunter.me>
2023-10-08 14:24:16 -0500

    Windows - install git alias, update Terminal settings

--- Windows/install-scoop-packages.ps1
+++ Windows/install-scoop-packages.ps1
@@ -5,3 +5,7 @@ scoop bucket add nerd-fonts # https://github.com/matthewjberger/scoop-nerd-fonts
 # Install hack font. Have to install for all users on Windows 11.
 # Requires admin privileges.
 scoop install -g Hack-NF Hack-NF-Mono
+
+# Git Aliases
+scoop bucket add extras
+scoop install git-aliases
