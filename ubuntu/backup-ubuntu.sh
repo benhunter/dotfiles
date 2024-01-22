d2251810927b88a7328ebc2b36b0a9343c474c35
Ben Hunter <code@benhunter.me>
2024-01-22 02:30:42 +0000

    Add ubuntu .zshrc and backup-ubuntu.sh

---
+++ ubuntu/backup-ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set +x
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+echo "Script Directory: $SCRIPT_DIR"
+
+echo "Copying .zshrc"
+cp ~/.zshrc $SCRIPT_DIR/
+
+git -C $SCRIPT_DIR switch main
+
+# if pull fails, exit
+if ! git -C $SCRIPT_DIR pull; then
+  echo "Pull failed. Exiting..."
+  exit 1
+fi
+
+git -C $SCRIPT_DIR add -A
+git -C $SCRIPT_DIR commit -v
+git -C $SCRIPT_DIR push
