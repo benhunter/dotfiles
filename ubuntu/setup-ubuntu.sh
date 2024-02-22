1d5557c577a805b20800e52d41c8a4e4522079ec
Ben Hunter <code@benhunter.me>
2024-02-22 02:18:37 +0000

    Improve setup-ubuntu.sh with self documenting + better unattended apt install

--- ubuntu/setup-ubuntu.sh
+++ ubuntu/setup-ubuntu.sh
@@ -9,47 +9,62 @@ then echo $LINE | (sudo su -c 'EDITOR="tee" visudo -f /etc/sudoers.d/user')
 fi;
 
 # apt
-sudo apt update
-sudo apt upgrade -y
+echo "Updating apt..."
+sudo DEBIAN_FRONTEND=noninteractive apt update
+echo "Upgrading apt..."
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 
-sudo apt install zsh -y
+echo "Installing zsh..."
+sudo DEBIAN_FRONTEND=noninteractive apt install -y zsh
+echo "Changing shell to zsh..."
 chsh -s $(which zsh)
 
 # mcso-aos
-sudo apt install aqemu make git gcc valgrind inotify-tools tree fd-find
+echo "Installing mcso-aos dependencies..."
+sudo apt install -y aqemu make git gcc valgrind inotify-tools tree fd-find
 
 # Oh My Zsh
+echo "Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # oh-my-zsh
 
 # PowerLevel10k
+echo "Installing PowerLevel10k..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # Set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc
 
 # .zshrc
+echo "Backing up and linking .zshrc..."
 mv $HOME/.zshrc $HOME/.zshrc.bak
 ln -s $SCRIPT_DIR/.zshrc $HOME/.zshrc
+echo "Linking .p10k.zsh..."
 ln -s $SCRIPT_DIR/.p10k.zsh $HOME/.p10k.zsh
 
 # zsh-autosuggestions
+echo "Installing zsh-autosuggestions..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions pull
 
+echo "Linking .zshrc..."
 ln -s $SCRIPT_DIR/.gitconfig $HOME/.gitconfig
 
 # Tmux
+echo "Linking .tmux.conf..."
 ln -s $SCRIPT_DIR/../.tmux.conf $HOME/.tmux.conf
+echo "Installing Tmux plugins..."
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 $HOME/.tmux/plugins/tpm/bin/update_plugins all
 
 # Rust
+echo "Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # bottom
+echo "Installing bottom..."
 cargo install bottom --locked
 
 # fnm and Node
-sudo apt install unzip
+sudo DEBIAN_FRONTEND=noninteractive apt install -y unzip
 curl -fsSL https://fnm.vercel.app/install | bash
 source $HOME/.zshrc
 fnm install 20
@@ -57,22 +72,26 @@ fnm install 20
 # Neovim
 # https://github.com/neovim/neovim/blob/master/INSTALL.md#ubuntu
 # Use unstable repo for PPA
-sudo apt-get install software-properties-common
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim python3-pip fonts-powerline ripgrep fd-find
+echo "Installing Neovim..."
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python-software-properties
+sudo DEBIAN_FRONTEND=noninteractive add-apt-repository ppa:neovim-ppa/unstable
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y neovim python3-pip fonts-powerline ripgrep fd-find
 
 ## Python prerequisites - docs may be outdated
 #sudo apt-get install python2-dev python-pip python3-dev python3-pip
 
 ## NvChad
 ## https://nvchad.com/docs/quickstart/install
+echo "Installing NvChad..."
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
-echo "Run NvChadUpdate and MasonInstallAll"
+echo "!!\n!!\n!!  Run NvChadUpdate and MasonInstallAll to finish NvChad setup\n!!\n!!\n!!"
 
 ## Neovim config for VSCode Remote
+echo "Copying Neovim config..."
 cp .config/nvim/init.lua $HOME/.config/nvim
 
 # fd
+echo "Linking fdfind to fd..."
 ln -s $(which fdfind) ~/.local/bin/fd
