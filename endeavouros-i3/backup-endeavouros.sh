23b72aa5abbd86ce8059e390bdabcfafd5bd4bc3
Ben Hunter <code@benhunter.me>
2024-03-13 20:16:22 -0600

    Backup ~/.config/i3

--- endeavouros-i3/backup-endeavouros.sh
+++ endeavouros-i3/backup-endeavouros.sh
@@ -3,4 +3,5 @@
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 cp ~/.Xresources "$SCRIPT_DIR"
 cp ~/.xinitrc "$SCRIPT_DIR"
-cp -r ~/.config/fontconfig "$SCRIPT_DIR"/.config/
+cp -r ~/.config/fontconfig "$SCRIPT_DIR/.config/"
+cp -r ~/.config/i3/ "$SCRIPT_DIR/.config/"
--- endeavouros-i3/i3-display-swap.sh
+++ endeavouros-i3/i3-display-swap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # https://gist.github.com/fbrinker/df9cfbc84511d807f45041737ff3ea02
 # requires jq
+#
+# To bind key, in i3 config (~/.config/i3/config) add:
+# bindsym $mod+Shift+s exec /path/to/your/script/i3-display-swap.sh
 
 if ! command -v jq &>/dev/null; then
     echo "jq could not be found"
@@ -18,19 +21,3 @@ for ROW in ${DISPLAY_CONFIG}; do
 done
 sleep 0.15
 i3-msg -- focus output "${ACTIVE_DISPLAY}"
-
-exit
-#### original ####
-# https://i3wm.org/docs/user-contributed/swapping-workspaces.html
-DISPLAY_CONFIG=($(i3-msg -t get_outputs | jq -r '.[]|"\(.name):\(.current_workspace)"'))
-
-for ROW in "${DISPLAY_CONFIG[@]}"
-do
-IFS=':'
-read -ra CONFIG <<< "${ROW}"
-if [ "${CONFIG[0]}" != "null" ] && [ "${CONFIG[1]}" != "null" ]; then
-    echo "moving ${CONFIG[1]} right..."
-    i3-msg -- workspace --no-auto-back-and-forth "${CONFIG[1]}"
-    i3-msg -- move workspace to output right	
-fi
-done
