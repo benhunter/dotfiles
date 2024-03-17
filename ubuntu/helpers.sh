84334b6f05a0af852482053b0633add3bb932a42
Ben Hunter <code@benhunter.me>
2024-03-17 20:59:04 +0000

    Fix path

--- ubuntu/helpers.sh
+++ ubuntu/helpers.sh
@@ -1,6 +1,6 @@
 # MCSO AOS Pintos Project 2 - User Programs
 make_check() {
-    cd ~/projects/aos_pintos_project_2/aos_pintos/
+    cd ~/projects/aos_pintos_project_2/aos_pintos/src/
     make clean
     cd userprog/
     make && make check
