d2448916b01b0c1ba122dc30d247b14d127e33e6
Ben Hunter <code@benhunter.me>
2024-04-07 14:39:16 -0600

    Add ssh config for X11 clipboard across ssh

--- endeavouros-i3/backup-endeavouros.sh
+++ endeavouros-i3/backup-endeavouros.sh
@@ -8,5 +8,6 @@ cp ~/.Xresources "$SCRIPT_DIR"
 cp ~/.xinitrc "$SCRIPT_DIR"
 cp -r ~/.config/fontconfig "$SCRIPT_DIR/.config/"
 cp -r ~/.config/i3/ "$SCRIPT_DIR/.config/"
+cp ~/.ssh/config "$SCRIPT_DIR/.ssh/config"
 
 git_commit
