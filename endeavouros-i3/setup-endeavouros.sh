70fb9dbce7afa6784f540f35bb69eae057b3b773
Ben Hunter <code@benhunter.me>
2024-04-14 14:00:09 -0500

    Update i3/config to use vim keys better + install latex, pdflatex

--- endeavouros-i3/setup-endeavouros.sh
+++ endeavouros-i3/setup-endeavouros.sh
@@ -63,7 +63,7 @@ sudo pacman -S ripgrep
 sudo pacman -S just
 sudo pacman -S noto-fonts-emoji
 sudo pacman -S xclip # allows Neovim to yank to system clipboard
-sudo pacman -S texlive-basic
+sudo pacman -S texlive-basic texlive-latex # texlive-formatsextra
 
 # Catppuccin
 # xfce4-terminal theme https://github.com/catppuccin/xfce4-terminal/blob/main/src/catppuccin-mocha.theme
