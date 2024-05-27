a026575226ec6fef1c68cf8cb483827d38f60e4c
Ben Hunter <code@benhunter.me>
2024-05-27 14:59:39 -0500

    Hyprland Dracula Theme, recursiveUpdate to merge attribute sets

--- nixos/home.nix
+++ nixos/home.nix
@@ -163,30 +163,30 @@ in
     };
   };
 
-  wayland.windowManager.hyprland = {
-    enable = true;
-    settings = {
-      "$mod" = "SUPER";
-      general = {
-        gaps_in = 3;
-        gaps_out = 5;
-      };
-      bind = [
-        "$mod, Q, killactive"
-        "$mod, Return, exec, kitty"
-        "$mod, F, exec, firefox"
+  wayland.windowManager.hyprland = let 
+    hyprlandConfig = {
+      enable = true;
+      settings = {
+        "$mod" = "SUPER";
+        general = {
+          gaps_in = 3;
+          gaps_out = 5;
+        };
+        bind = [
+          "$mod, Q, killactive"
+          "$mod, Return, exec, kitty"
+          "$mod, F, exec, firefox"
 
-        "$mod, H, movefocus, l"
-        "$mod, J, movefocus, d"
-        "$mod, K, movefocus, u"
-        "$mod, L, movefocus, r"
+          "$mod, H, movefocus, l"
+          "$mod, J, movefocus, d"
+          "$mod, K, movefocus, u"
+          "$mod, L, movefocus, r"
 
-        ", Print, exec, grimblast copy area"
-      ]
-      ++ (
+          ", Print, exec, grimblast copy area"
+        ] ++ (
         # workspaces
         # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-        builtins.concatLists (builtins.genList (
+          builtins.concatLists (builtins.genList (
             x: let
               ws = let
                 c = (x + 1) / 10;
@@ -196,16 +196,41 @@ in
               "$mod, ${ws}, workspace, ${toString (x + 1)}"
               "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
             ]
-          )
-          10)
-      );
-
-      misc = {
-        disable_hyprland_logo = true;
+          ) 10)
+        );
+        misc = {
+          disable_hyprland_logo = true;
+        };
+        exec-once = "waybar";
       };
-      exec-once = "waybar";
-    };
-  };
+    }; # end hyprlandConfig
+
+    hyprlandThemeDracula = {
+    # https://github.com/dracula/hyprland/blob/main/hyprland.conf
+      settings = {
+        general = {
+          "col.active_border" = "rgb(44475a) rgb(bd93f9) 90deg";
+          "col.inactive_border" = "rgba(44475aaa)";
+          "col.nogroup_border" = "rgba(282a36dd)";
+          "col.nogroup_border_active" = "rgb(bd93f9) rgb(44475a) 90deg";
+          no_border_on_floating = false;
+          border_size = 2;
+        };
+        decoration = {
+          "col.shadow" = "rgba(1E202966)";
+        };
+        group = {
+          groupbar = {
+            "col.active" = "rgb(bd93f9) rgb(44475a) 90deg";
+            "col.inactive" = "rgba(282a36dd)";
+          };
+        };
+      };
+    }; # end hyprlandThemeDracula
+
+  in
+    lib.recursiveUpdate hyprlandConfig hyprlandThemeDracula;
+
 
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
