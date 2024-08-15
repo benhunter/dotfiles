9f554f67dd7e33b6e54eb0a8125b81946906bfd5
Ben Hunter <code@benhunter.me>
2024-08-15 06:23:57 +0000

    Add GDB rpyc info for using pwntools with GDB python API.

--- kali-live/setup-kali.sh
+++ kali-live/setup-kali.sh
@@ -5,3 +5,8 @@ ln -s ~/projects/dotfiles/.tmux.conf ~/.tmux.conf
 # if clamav user no longer exists:
 # sudo adduser --system --no-create-home --disabled-password --disabled-login --shell /bin/false --group --home /var/lib/clamav clamav
 
+# rpyc Python package for gdb
+# check gdb's python path and rpyc avalailability:
+# gdb -batch -ex 'python print(sys.path)'
+# gdb -batch -ex 'python import rpyc'
+# sudo pip install rpyc --target /usr/share/gdb/python
