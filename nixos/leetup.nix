c57ea103bcbbf4a9122798c94287abccfaa6ff7c
Ben Hunter <code@benhunter.me>
2024-10-12 00:51:27 -0500

    Nixos: WIP leetup

--- nixos/leetup.nix
+++ nixos/leetup.nix
@@ -1,5 +1,7 @@
 { pkgs }:
 
+# https://chatgpt.com/c/6709ec7f-90d4-800a-b17b-cbac36634277
+
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "leetup";
   version = "v1.2.5";
@@ -7,9 +9,9 @@ pkgs.rustPlatform.buildRustPackage rec {
   src = pkgs.fetchFromGitHub {
     owner = "benhunter";
     repo = "leetup";
-    rev = "";
-    sha256 = "";
+    rev = "ff67ebf";
+    sha256 = "sha256-iivyOATeEKMTVxrmd/qvN6mBxgPDZns3LHXhXYhd2xM=";
   };
 
-  cargoSha256 = "";
+  cargoSha256 = "sha256-doQkRqRMM1Nf/h+65rzklkwlrzI8RpYrhAGMX6JM4GU=";
 }
