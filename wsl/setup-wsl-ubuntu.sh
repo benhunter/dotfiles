b7da237c3b93acdfe6f6bc84ef79d308066f8b1e
Ben Hunter <code@benhunter.me>
2025-03-31 18:57:40 -0500

    wsl: check for brew before installing

--- wsl/setup-wsl-ubuntu.sh
+++ wsl/setup-wsl-ubuntu.sh
@@ -1,12 +1,15 @@
 # TODO direnv not found
 
+has() { command -v "$1" >/dev/null 2>&1; }
+
 # TODO setup-ubuntu.sh not found when this script is run from another folder.
 ./../ubuntu/setup-ubuntu.sh
 
 # Homebrew
 # https://brew.sh/
-# TODO skip if homebrew is already installed.
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! has brew; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Docker permissions
 sudo usermod -aG docker $USER
