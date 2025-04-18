7f267b95cdca6d878e73c9204ea186dfb3b68dc4
Ben Hunter <code@benhunter.me>
2025-04-18 12:23:32 -0500

    Ubuntu: install kubectl script

---
+++ ubuntu/install-kubectl.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Install kubectl on Debian based systems.
+# 2025-04-25
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+
+# Alternative: Homebrew
+# brew install kubectl
+# kubectl version --client
+
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+
+# If the folder `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
+# sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+
+sudo apt-get update
+sudo apt-get install -y kubectl
