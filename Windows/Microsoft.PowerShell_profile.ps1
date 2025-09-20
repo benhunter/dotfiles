a75fae451064e240ab27a250d1fca87c0e5109c3
Ben Hunter <code@benhunter.me>
2025-09-20 12:26:48 -0500

    windows: update powershell profile

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -19,6 +19,7 @@ function Watch-Command {
 }
 
 Set-Alias -Name watch -Value Watch-Command
+Set-Alias -Name w -Value Watch-Command
 
 Import-Module git-aliases -DisableNameChecking
 
@@ -28,3 +29,16 @@ function gst {
 }
 
 fnm env --use-on-cd | Out-String | Invoke-Expression
+
+# IPFS Kubo
+[System.Environment]::SetEnvironmentVariable('PATH',$Env:PATH+';;C:\Program Files\kubo_v0.24.0\kubo')
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
