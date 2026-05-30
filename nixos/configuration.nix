8ecbe91ce8bcb5722f1e1b25a27fc0c168b5a151
Ben Hunter <code@benhunter.me>
2026-05-29 22:25:42 -0500

    nixos: fix tuigreet

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -61,7 +61,7 @@
   services.greetd = {
     enable = true;
     settings.default_session = {
-      command = "${pkgs.tuigreet}/bin/tuigreet --time -- cmd Hyprland";
+      command = "${pkgs.tuigreet}/bin/tuigreet --time";
       user = "greeter";
     };
   };
