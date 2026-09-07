7bca086ffa545aa765ebcac9c1f042cbb6fd647f
Ben Hunter <code@benhunter.me>
2026-09-06 22:02:28 -0500

    spidey: install podman + run uptime-kuma

--- spidey-raspi5/setup.sh
+++ spidey-raspi5/setup.sh
@@ -1,3 +1,6 @@
+echo "Installing Podman..."
+./install-podman.sh
+
 echo "Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
@@ -7,3 +10,6 @@ sudo apt install build-essential
 
 echo "Installing topgrade..."
 cargo install topgrade
+
+echo "Running Uptime Kuma..."
+podman run -d --restart=always -p 3001:3001 -v uptime-kuma:/app/data --name uptime-kuma docker.io/louislam/uptime-kuma:2
